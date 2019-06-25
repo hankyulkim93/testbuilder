6a0cb359b77249aee9ba157b2b4e16f0bfba99af
Han Kim <hankyulkim93@gmail.com>
2019-06-24 21:10:13 -0700

    Added China UnionPay and Switch (complete)

--- detectNetwork.js
+++ detectNetwork.js
@@ -12,8 +12,21 @@ var detectNetwork = function(cardNumber) {
   var prefixTwo = cardNumber.substring(0,2);
   var prefixThree = cardNumber.substring(0,3);
   var prefixFour = cardNumber.substring(0,4);
+  var prefixSix = cardNumber.substring(0,6);
   var cardLength = cardNumber.length;
 
+//Switch
+  if (prefixFour === '4903' || prefixFour === '4905' || prefixFour === '4911' || prefixFour === '4936' || prefixFour === '6333' || prefixFour === '6759') {
+    if (cardLength === 16 || cardLength === 18 || cardLength === 19) {
+      return 'Switch'
+    };
+  } else if (prefixSix === '564182' || prefixSix === '633110') {
+    if (cardLength === 16 || cardLength === 18 || cardLength === 19) {
+      return 'Switch';
+    };
+  };
+
+//Diner's Club, American Express, Visa, MasterCard, Maestro
   if ( (prefixTwo === '38' || prefixTwo === '39') && cardLength === 14 ) {
       return 'Diner\'s Club';
   } else if ( (prefixTwo === '34' || prefixTwo === '37') && cardLength === 15 ) {
@@ -22,21 +35,40 @@ var detectNetwork = function(cardNumber) {
       return 'Visa';
   } else if ( (prefixTwo === '51' || prefixTwo === '52' || prefixTwo === '53' || prefixTwo === '54' || prefixTwo === '55') && cardLength === 16) {
       return 'MasterCard';
-  } else if ((prefixFour === '5018' || prefixFour === '5020' || prefixFour === '5038' || prefixFour === '6304') && (cardLength === 12 || cardLength === 13 || cardLength === 14 || cardLength === 15 || cardLength === 16 || cardLength === 17 || cardLength === 18 || cardLength === 19) ) {
+  } else if ( (prefixFour === '5018' || prefixFour === '5020' || prefixFour === '5038' || prefixFour === '6304') && (cardLength === 12 || cardLength === 13 || cardLength === 14 || cardLength === 15 || cardLength === 16 || cardLength === 17 || cardLength === 18 || cardLength === 19) ) {
       return 'Maestro';
-  } else if (prefixFour === '6011' || prefixTwo === '65' || 644 <= prefixThree <= 649) {
+  };
+
+//Discover
+  if(prefixFour === '6011') {
       if (cardLength === 16 || cardLength === 19) {
         return 'Discover';
-      }
+      };
+  } else if (prefixTwo === '65') {
+      if (cardLength === 16 || cardLength === 19) {
+        return 'Discover';
+      };
+  } else if (644 <= prefixThree && prefixThree <= 649) {
+      if (cardLength === 16 || cardLength === 19) {
+      return "Discover";
+    };
+  };
 
-  } else {
-      return 'Invalid';
-  }
-  // Note: `cardNumber` will always be a string
-  // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
-  // The American Express network always starts with a 34 or 37 and is 15 digits long
-
-  // Once you've read this, go ahead and try to implement this function, then return to the console.
+//China UnionPay
+  if (622126 <= prefixSix <= 622925) {
+      if (16 <= cardLength <= 19) {
+        return 'China UnionPay';
+      };
+  } else if (624 <= prefixThree <= 626) {
+      if (16 <= cardLength <= 19) {
+        return 'China UnionPay';
+      };
+  } else if (6282 <= prefixFour <= 6288) {
+      if (16 <= cardLength <= 19) {
+        return 'China UnionPay';
+      };
+  };
 };
+
 
 
